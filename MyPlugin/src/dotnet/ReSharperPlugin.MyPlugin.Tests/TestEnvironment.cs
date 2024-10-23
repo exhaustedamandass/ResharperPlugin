47be51fe8404c0a5c4121cf6ef398998ce277df1
Assanali Amandykov <amandass@fit.cvut.cz>
2024-10-23 13:43:41 +0200

    template

---
+++ MyPlugin/src/dotnet/ReSharperPlugin.MyPlugin.Tests/TestEnvironment.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+using JetBrains.Application.BuildScript.Application.Zones;
+using JetBrains.ReSharper.Feature.Services;
+using JetBrains.ReSharper.Psi.CSharp;
+using JetBrains.ReSharper.TestFramework;
+using JetBrains.TestFramework;
+using JetBrains.TestFramework.Application.Zones;
+using NUnit.Framework;
+
+[assembly: Apartment(ApartmentState.STA)]
+
+namespace ReSharperPlugin.MyPlugin.Tests
+{
+    [ZoneDefinition]
+    public class MyPluginTestEnvironmentZone : ITestsEnvZone, IRequire<PsiFeatureTestZone>, IRequire<IMyPluginZone> { }
+
+    [ZoneMarker]
+    public class ZoneMarker : IRequire<ICodeEditingZone>, IRequire<ILanguageCSharpZone>, IRequire<MyPluginTestEnvironmentZone> { }
+
+    [SetUpFixture]
+    public class MyPluginTestsAssembly : ExtensionTestEnvironmentAssembly<MyPluginTestEnvironmentZone> { }
+}
