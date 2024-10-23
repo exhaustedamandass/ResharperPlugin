638182f74cf3e145ad574ec4838a9f32de4aeb5e
Assanali Amandykov <amandass@fit.cvut.cz>
2024-10-23 14:03:27 +0200

    OptionsPage Implemented

---
+++ MyPlugin/src/dotnet/ReSharperPlugin.MyPlugin/Options/OptionsPage.cs
@@ -0,0 +1,35 @@
+using System;
+using System.ComponentModel;
+using JetBrains.Application.Settings.WellKnownRootKeys;
+using JetBrains.Application.UI.Options;
+using JetBrains.Application.UI.Options.OptionPages;
+using JetBrains.Application.UI.Options.OptionsDialog;
+using JetBrains.DataFlow;
+using JetBrains.IDE.UI.Options;
+using JetBrains.Lifetimes;
+using JetBrains.ReSharper.Feature.Services.Resources;
+
+namespace ReSharperPlugin.MyPlugin.Options;
+
+//can play with the icon
+[OptionsPage(Pid, "R# Git plugin", typeof(FeaturesEnvironmentOptionsThemedIcons.GeneratedCode), ParentId = ToolsPage.PID)]
+public class OptionsPage : BeSimpleOptionsPage
+{
+    private const string Pid = "ReSharperGitPluginOptions";
+    
+    [Obsolete("Obsolete")]
+    public OptionsPage(Lifetime lifetime, OptionsPageContext optionsPageContext,
+        OptionsSettingsSmartContext optionsSettingsSmartContext, bool wrapInScrollablePanel = false) : base(lifetime,
+        optionsPageContext, optionsSettingsSmartContext, wrapInScrollablePanel)
+    {
+        IProperty<int> nCommits = new Property<int>(lifetime, "ReSharperGitPluginOptions::nCommits");
+        nCommits.SetValue(optionsSettingsSmartContext.StoreOptionsTransactionContext.GetValue( (MySettingsKey key) => key.NCommits));
+    }
+}
+
+[JetBrains.Application.Settings.SettingsKey(typeof(EnvironmentSettings), "My settings")]
+public class MySettingsKey
+{
+    [JetBrains.Application.Settings.SettingsEntry(false, "Check Me")]
+    public int NCommits;
+}
