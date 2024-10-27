6f91fee0a0ebb43360c35c7033c3212e3ba56637
Assanali Amandykov <amandass@fit.cvut.cz>
2024-10-27 02:18:41 +0200

    implemented highlighting logic and classes

---
+++ MyPlugin/src/dotnet/ReSharperPlugin.MyPlugin/ElementProblemAnalyzers/ClassModificationInfo.cs
@@ -0,0 +1,26 @@
+using System;
+using JetBrains.DocumentModel;
+using JetBrains.ReSharper.Daemon.Specific.Errors.Internal;
+using JetBrains.ReSharper.Feature.Services.Daemon;
+
+namespace ReSharperPlugin.MyPlugin.ElementProblemAnalyzers;
+
+//TODO: fix this
+[StaticSeverityHighlighting(Severity.INFO, HighlightingGroupIds.CodeInfo)]
+public class ClassModificationInfo : IHighlighting
+{
+    public bool IsValid()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public DocumentRange CalculateRange()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    //TODO: should return the corresponding commit message.
+    public string ToolTip => throw new NotImplementedException();
+    
+    public string ErrorStripeToolTip { get; }
+}
